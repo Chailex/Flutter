b35c260bc0f3746586af3d8228682d5fcb09adfb
chailex <chailexsarma@gmail.com>
2022-12-28 01:50:29 +0530

    Day2completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:my_appthirty/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
+  final String name = "Chailex";
+  final double number = 3.14;
+  final bool isMale = true;
+  final num temp = 30.5;
+
+  final double pi = 3.14;
+  // final  //difference between final and const is that we make some changes in final whereas in const we can't.
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
